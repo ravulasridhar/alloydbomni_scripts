1317f83703d5715e0cde7453fb0aa5dafeb781f8
ravulasridhar <50375107+ravulasridhar@users.noreply.github.com>
2025-01-31 20:36:24 -0500

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -2,3 +2,4 @@
 ## Create user alloydbomni. This user will be used on all VM's to run the alloydb software.
 sudo useradd -s /bin/bash -u 1500 -m -G adm,video alloydbomni
 sudo usermod -a -G wheel alloydbomni
+sudo usermod -a -G google-sudoers alloydbomni
